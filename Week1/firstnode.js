7cd376a3fab11492122f38fe0974acaf0c1f4030
MetroBeann <caseycompulsory@gmail.com>
2024-04-18 22:11:17 +0800

    testing on first node.js and how to view using localhost

---
+++ Week1/firstnode.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 3030;
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Really world?');
+});
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
