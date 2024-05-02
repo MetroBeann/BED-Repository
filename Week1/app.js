20b63cc5ae084700aa6e640b7d19fe03118cd659
MetroBeann <caseycompulsory@gmail.com>
2024-05-02 10:20:16 +0800

    completed wk 1 & 2

--- Week1/app.js
+++ Week1/app.js
@@ -2,10 +2,11 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3030;
 const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Really world?');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World ');
 });
+
 server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
